63ed6402175dc0af8d83e731539cccce02215c5b
Pim Kartner <p.kartner@yonego.nl>
2017-10-28 20:46:40 +0200

    error checks

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -81,7 +80,7 @@ func main() {
 
 	bucket, err = cluster.OpenBucket("example", "")
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 	router.HandleFunc("/create", CreateEndpoint).Methods("POST")
 	router.HandleFunc("/expand/", ExpandEndpoint).Methods("GET")
